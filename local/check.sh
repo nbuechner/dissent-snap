fcdadb64259b0e5fbd5fa0d8361918e17183cb9f
Nils Büchner <n.buechner@united-gameserver.de>
2024-01-15 13:59:20 +0100

    - remove dbus system service - add permission info

---
+++ local/check.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+snapctl is-connected password-manager-service
+pms=$?
+
+echo "password-manager-service $pms"
+
+if [ "$pms" == "0" ]; then
+   exec "${@}"
+   exit 0
+fi
+
+if [ -f $SNAP_USER_COMMON/.asked-permissions ]; then
+   exec "${@}"
+   exit 0
+fi
+
+xdg-open https://buechner.me/gtkcord4/permissions.html
+
+touch $SNAP_USER_COMMON/.asked-permissions
+
+exec "${@}"
